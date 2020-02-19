dfedcebaae2a4fb54d5b65bbbcb2fb3b8b39ec39
Lucas Moraes <lucasmoraes2@outlook.com.br>
2020-02-18 23:51:26 -0300

    Lógica

--- aula48/index.js
+++ aula48/index.js
@@ -1,6 +1,33 @@
 // Declaração de função (Function hoisting)
-
 falaOi();
 function falaOi() {
     console.log('Oi');
 }
+
+// First-class objects (Objetos de primeira classe) - todas
+// Function expression
+const souUmDado = function() {
+    console.log('Sou um dado.');
+};
+souUmDado();
+
+// function executaFuncao(funcao) {
+//     console.log('Vou executa sua função abaixo:');
+//     funcao();
+// }
+// executaFuncao(souUmDado);
+
+// Arrow function
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function');
+};
+funcaoArrow();
+
+// Dentro de um objeto
+const obj = {
+    falar() { // falar: function() {
+        console.log('Estou falando...');
+    }
+};
+
+obj.falar();
