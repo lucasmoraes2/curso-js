cc0f98f66e56f4234dcedd9c848d69c87da103a2
Lucas Moraes <lucasmoraes2@outlook.com.br>
2020-04-14 15:34:47 -0300

    Projeto agenda

--- projetoagenda/src/controllers/contatoController.js
+++ projetoagenda/src/controllers/contatoController.js
@@ -6,7 +6,7 @@ exports.index = (req, res) => {
     });
 };
 
-exports.register = async (req, res) => {
+exports.register = async(req, res) => {
     try {
         const contato = new Contato(req.body);
         await contato.register();
@@ -54,4 +54,14 @@ exports.edit = async function(req, res) {
         console.log(e);
         res.render('404');
     }
+};
+
+exports.delete = async function(req, res) {
+    if(!req.params.id) return res.render('404');
+
+    const contato = await Contato.delete(req.params.id);
+    if(!contato) return res.render('404');
+
+    req.flash('success', 'Contato apagado com sucesso.');
+    req.session.save(() => res.redirect('back'));
 };
