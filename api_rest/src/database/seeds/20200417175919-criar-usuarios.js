7ef05893f00a5da09afb869bb62a6909f3b7e778
Lucas Moraes <lucasmoraes2@outlook.com.br>
2020-04-17 15:11:29 -0300

    Api rest

---
+++ api_rest/src/database/seeds/20200417175919-criar-usuarios.js
@@ -0,0 +1,33 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert(
+    'users',
+    [
+      {
+        nome: 'Aurora',
+        email: 'aurora@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Nilce',
+        email: 'nilce@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Tracy',
+        email: 'tracy@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ],
+    {},
+  ),
+
+  down: () => {},
+};
