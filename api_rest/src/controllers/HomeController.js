34a15b8e246f03e2b165a3e8326b6bb90b47f6b8
Lucas Moraes <lucasmoraes2@outlook.com.br>
2020-04-17 15:38:05 -0300

    Api rest

--- api_rest/src/controllers/HomeController.js
+++ api_rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Aurora',
-      sobrenome: 'Aksnes',
-      email: 'aurora@email.com',
-      idade: 23,
-      peso: 60,
-      altura: 1.63,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
